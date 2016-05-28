52db856fe14fc753c93e9134b4ec16d02cfb9265
blakflagg <dduffy2@outlook.com>
2016-05-28 09:44:12 -0700

    modified the front end form to send messages

--- public/js/app.js
+++ public/js/app.js
@@ -8,5 +8,18 @@ socket.on('connect',function(){
 socket.on('message',function(message){
   console.log('New Message: ');
   console.log(message.text);
-  
-})
+
+});
+
+//handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event){
+  event.preventDefault();
+
+  socket.emit('message',{
+    text: $form.find('input[name=message]').val()
+  });
+
+  $form.find('input[name=message]').val("");
+});

e7898d6f228b744a2dccbe0ffc418bd207a4fb6c
blakflagg <dduffy2@outlook.com>
2016-05-28 13:39:27 -0700

    updated server and frontend ui to show timestamp

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,18 @@
 var socket = io();
-
+var now = moment();
 socket.on('connect',function(){
   console.log('connected to server');
 
 });
 
 socket.on('message',function(message){
+
+
   console.log('New Message: ');
   console.log(message.text);
+  console.log( now.format('h:mm a'));
+  jQuery('.messages').append('<p>'+'<strong>' +  moment().local().utc(message.timestamp).format('h:mm a') + ' > ' + '</strong>' + message.text +'</p>');
 
-  jQuery('.messages').append('<p>'+ message.text +'</p>');
-  
 });
 
 //handles submitting of new message
@@ -20,7 +22,8 @@ $form.on('submit', function(event){
   event.preventDefault();
 
   socket.emit('message',{
-    text: $form.find('input[name=message]').val()
+    text: $form.find('input[name=message]').val(),
+    timestamp: now.valueOf('x')
   });
 
   $form.find('input[name=message]').val("");
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
+var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,13 +12,14 @@ io.on('connection',function(socket){
   console.log('user connected via socket.io!');
 
   socket.on('message', function(message){
-    console.log('Message received ' + message.text);
+    console.log('Message received ' + moment().local().utc(message.timestamp).format('h:mm a') + '> ' + message.text);
     //socket.broadcast.emit('message', message); //sends the message to all except the sender
     io.emit('message', message);
   });
 
   socket.emit('message',{
-    text: 'Welcome to the chat application'
+    text: 'Welcome to the chat application',
+    timestamp: now.valueOf('x')
   });
 }); //listen for events
 

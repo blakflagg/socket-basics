e71dfb5abf9253fe53a76f8e40364170d9c9649d
blakflagg <dduffy2@outlook.com>
2016-05-28 09:31:41 -0700

    added basic message broadcasting to client and server

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,11 @@ var socket = io();
 
 socket.on('connect',function(){
   console.log('connected to server');
+
 });
+
+socket.on('message',function(message){
+  console.log('New Message: ');
+  console.log(message.text);
+  
+})
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection',function(){
+io.on('connection',function(socket){
   console.log('user connected via socket.io!');
+
+  socket.on('message', function(message){
+    console.log('Message received ' + message.text);
+    socket.broadcast.emit('message', message); //sends the message to all except the sender
+  });
+
+  socket.emit('message',{
+    text: 'Welcome to the chat application'
+  });
 }); //listen for events
 
 http.listen(PORT,function(){

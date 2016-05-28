61bdb1161542b35ea21eac703679a6de68ee8b7f
blakflagg <dduffy2@outlook.com>
2016-05-28 15:04:34 -0700

    added disconnect event and message

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ var clientInfo = {};
 io.on('connection',function(socket){
   console.log('user connected via socket.io!');
 
+  socket.on('disconnect', function(){
+    var userData = clientInfo[socket.id];
+
+    if(typeof userData !== 'undefined'){
+      socket.leave(userData.room);
+      io.to(userData.room).emit('message',{
+        name: 'System',
+        text: userData.name + ' has left the room.',
+        timestamp: moment().valueOf()
+      });
+      delete userData;
+    }
+  });
   socket.on('joinRoom', function(req){
     clientInfo[socket.id] = req;
     socket.join(req.room);
